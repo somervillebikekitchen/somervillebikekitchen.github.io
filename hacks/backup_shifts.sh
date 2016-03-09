d2902a6b2f1ed01e2aa41404631889585778019d
Troy Sankey <sankeytms@gmail.com>
2016-03-09 18:25:00 -0500

    implement Terms Matrix

--- hacks/backup_shifts.sh
+++ hacks/backup_shifts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 date=$(date --iso)
 wget -O sbk_shifts_$date.csv http://sbk.sankey.info/calc/dewxjhhcc2.csv
+wget -O sbk_terms_$date.csv http://sbk.sankey.info/calc/sbk_start_dates.csv
+
